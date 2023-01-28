a2d203e9e4f9f117c93325763b3824eb8cb67aae
Milchess <milch.ks.2992@gmail.com>
2023-01-28 21:25:53 +0500

    white list cors

--- backend/constants.js
+++ backend/constants.js
@@ -5,6 +5,11 @@ const allowedCors = [
   'http://praktikum.tk',
   'https://localhost:3000',
   'http://localhost:3000',
+  'http://51.250.25.171:3000',
+  'https://milchess.nomoredomainsclub.ru',
+  'http://milchess.nomoredomainsclub.ru',
+  'https://api.milchess.nomoredomainsclub.ru',
+  'http://api.milchess.nomoredomainsclub.ru',
   'localhost:3000',
 ];
 
