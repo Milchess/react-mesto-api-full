e52c42e6baad32807d9d78d71b715230354bbae9
Milchess <milch.ks.2992@gmail.com>
2023-01-29 13:01:06 +0500

    отмена лайков

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,13 +4,13 @@ import {CurrentUserContext} from "../contexts/CurrentUserContext";
 function Card(props) {
     const currentUser = useContext(CurrentUserContext);
 
-    const isOwn = props.card.owner._id === currentUser._id;
+    const isOwn = props.card.owner === currentUser._id;
 
     const cardDeleteButtonClassName = (
         `grid-card__delete ${isOwn ? 'grid-card__delete_visible' : 'grid-card__delete_hidden'}`
     );
 
-    const isLiked = props.card.likes.some(i => i._id === currentUser._id);
+    const isLiked = props.card.likes.some(i => i === currentUser._id);
 
     const cardLikeButtonClassName = `grid-card__like ${isLiked && 'grid-card__like_active'}`;
 
